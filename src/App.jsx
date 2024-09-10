fd3ff299b9791cd18b573f990f18aaa10c5da3fb
Style <esrsamarth@gmail.com>
2024-09-10 10:21:10 +0530

    a map function to render notes is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Note from "./component/Note";
+import notes from "./notes";
+
+const createNotes = (noteItem) => <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+/>
+
+
+
 export default function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
